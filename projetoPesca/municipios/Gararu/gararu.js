a7c49a2492d2d71e2b27c7d9a6c0ed930bb8bcb7
João Vítor dos Santos Almeida <jvsalmeida@sefaz.se.gov.br>
2024-08-09 12:19:20 -0300

    refactor: Refatora relatos dos pescadores

--- projetoPesca/municipios/Gararu/gararu.js
+++ projetoPesca/municipios/Gararu/gararu.js
@@ -2,16 +2,18 @@
 document.addEventListener("DOMContentLoaded", init, false);
 
 var relatoPescador;
+var relatoPescadorContagem;
 
 const texts = [
-    "Comer peixe é bom, mas comer tilápia é melhor",
-    "Água mole pedra dura, tanto bate até que fura",
-    "Abacate quando nasce, se esparrama pelo chão"
+    "O rio, acabou aquelas crôa, aquelas areiona que tinha, muita sujeira, o pessoal joga. Pronto, às vezes eu vou no rio bem cedo, porque eu gosto de lavar roupa, eu tenho um tanquinho, mas eu gosto de lavar mais no rio. Eu saio 5 horas, a gente cata, junta, eu tenho um amigo que pesca mais pra cá, a gente deixa no lixo, eu levo até um saco e trago.",
+    "Frase 2"
 ]
 
 async function init() {
     relatoPescador = document.getElementById("relatoPescador");
+    relatoPescadorContagem = document.getElementById("relatoPescadorContagem");
     relatoPescador.innerText = texts[0];
+    relatoPescadorContagem.innerText = "1/" + texts.length;
 }
 
 function nextText(){
@@ -20,6 +22,8 @@ function nextText(){
     } else {
         relatoPescador.innerText = texts[0]
     }
+
+    relatoPescadorContagem.innerText = getCurrentText()+1 + "/" + texts.length
 }
 
 function prevText(){
@@ -28,6 +32,8 @@ function prevText(){
     } else {
         relatoPescador.innerText = texts[texts.length-1]
     }
+
+    relatoPescadorContagem.innerText = getCurrentText()+1 + "/" + texts.length
 }
 
 function getCurrentText(){
