eee64c6ab85fe13011379df22ea25cd236ea8725
GabrielDosSantosAlmeida <bieljvsa@gmail.com>
2024-08-09 13:28:34 -0300

    feat: adição das frases

--- projetoPesca/municipios/Gararu/gararu.js
+++ projetoPesca/municipios/Gararu/gararu.js
@@ -5,10 +5,10 @@ var relatoPescador;
 var relatoPescadorContagem;
 
 const texts = [
-    "O rio, acabou aquelas crôa, aquelas areiona que tinha, muita sujeira, o pessoal joga. Pronto, às vezes eu vou no rio bem cedo, porque eu gosto de lavar roupa, eu tenho um tanquinho, mas eu gosto de lavar mais no rio. Eu saio 5 horas, a gente cata, junta, eu tenho um amigo que pesca mais pra cá, a gente deixa no lixo, eu levo até um saco e trago.",
-    "Frase 2",
-    "frase 3",
-    "frase 4"
+    "“O rio, acabou aquelas crôa, aquelas areiona que tinha, muita sujeira, o pessoal joga. Pronto, às vezes eu vou no rio bem cedo, porque eu gosto de lavar roupa, eu tenho um tanquinho, mas eu gosto de lavar mais no rio. Eu saio 5 horas, a gente cata, junta, eu tenho um amigo que pesca mais pra cá, a gente deixa no lixo, eu levo até um saco e trago.”",
+    "“Então se a gente tivesse consciência de que aquilo ali é da gente mesmo e que a gente precisa e que o benefício é pra gente, já resolvia muito”",
+    "“Se a associação desse sua ajuda melhor, o pescador daria pra dar uma forcinha, você comprar uma rede, mas que quando acaba a rede se torna caro pra você comprar, mas o jeito, compra apusso mas tem que comprar, aí ajeitar um barco, essas coisas.”",
+    "“Não tem outra forma de renda aqui. Se você não tiver emprego, aqui não tem emprego, é pouco. Aí todo mundo corre pra pesca.”"
 ]
 
 async function init() {
